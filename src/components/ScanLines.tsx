7c4ab37f47057e56118669bdbb90f00926a32ac0
Clonk <pauliusofficial4@gmail.com>
2026-02-04 00:18:16 +0000

    Add src/components/ScanLines.tsx

---
+++ src/components/ScanLines.tsx
@@ -0,0 +1,9 @@
+export function ScanLines() {
+  return (
+    <>
+      <div className="scan-lines"></div>
+      <div className="vignette"></div>
+      <div className="noise-overlay"></div>
+    </>
+  );
+}
